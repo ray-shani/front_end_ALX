f590cdc8530240310ea57f34160eac5de4aca45c
Ray Shani <raysainepushani@gmail.com>
2025-08-30 18:27:24 +0300

    update:social login updates via javascript

---
+++ alx_registration_app/src/componets/Sociallogin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SocialLogin.css';
+
+const SocialLogin = () => {
+  return (
+    <div className="social-login">
+      <div className="social-icon google"></div>
+      <div className="social-icon apple"></div>
+      <div className="social-icon facebook"></div>
+    </div>
+  );
+};
+
+export default SocialLogin;
