51f53231e5a27c75f65b79670d624114c56c0a09
Ray Shani <raysainepushani@gmail.com>
2025-08-30 18:55:50 +0300

    update: changing routes for the paths

--- alx_registration_app/src/App.js
+++ alx_registration_app/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import ForgotPassword from './components/ForgotPassword/ForgotPassword';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot" element={<ForgotPassword />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
