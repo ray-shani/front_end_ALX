18195040282117580c3ec195aa640cf658902479
Ray Shani <raysainepushani@gmail.com>
2025-08-31 17:29:45 +0300

    update: adding the input .jsx files

---
+++ alx_registration_app/src/Components/Shared/SocialLogin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SocialLogin.css';
+
+const SocialLogin = () => {
+  return (
+    <div className="social-login">
+      <div className="social-icon google">G</div>
+      <div className="social-icon apple">A</div>
+      <div className="social-icon facebook">F</div>
+    </div>
+  );
+};
+
+export default SocialLogin;
