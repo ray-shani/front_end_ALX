ae1914cce70190341a6df71834ad15170b332c83
Ray Shani <raysainepushani@gmail.com>
2025-08-31 21:40:54 +0300

    update: updatingthe routes

--- alx_registration_app/src/App.jsx
+++ alx_registration_app/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css';
 import Login from './components/Login/Login.jsx';
 import Register from './components/Register/Register.jsx';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword.jsx';
+import Landing from './components/Landing/Landing.jsx';
+import CheckIn from './components/CheckIn/CheckIn.jsx';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot" element={<ForgotPassword />} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/checkin/:role" element={<CheckIn />} />
         </Routes>
       </div>
     </Router>
